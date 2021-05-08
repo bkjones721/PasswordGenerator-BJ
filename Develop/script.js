3bb574323687abac84f3a745c5979c7d77370233
Brian Jones <bkjones721@gmail.com>
2021-05-08 16:35:17 -0400

    added conditions

--- Develop/script.js
+++ Develop/script.js
@@ -15,12 +15,30 @@ function generatePassword() {
   const lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   const specialCharacters = ['!', '@', '#', '$'];
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  let combinedChars = []
   let yesUpperCase = confirm("Do you like Upper case letters ?")
   let yesLowerCase = confirm("Do you like Lower case letters ?")
   let yesSpecialCharacters = confirm("Do you like special characters ?")
-  let number = confirm("Do you like numbers ?")
+  let yesNumbers = confirm("Do you like numbers ?")
   let lengthOfPassword = prompt("How long do you want your password ?")
-  const combinedChars = capitalCaseLetters + lowerCaseLetters + specialCharacters + numbers;
+  if (yesUpperCase) {
+    combinedChars = combinedChars.concat(capitalCaseLetters)
+    // console.log(yesUpperCase,combinedChars)
+  }
+  if (yesLowerCase) {
+    combinedChars = combinedChars.concat(lowerCaseLetters)
+  }
+  if (yesSpecialCharacters) {
+    combinedChars = combinedChars.concat(specialCharacters)
+  }
+  if (yesNumbers) {
+    combinedChars = combinedChars.concat(numbers)
+  }
+  if (combinedChars.length === 0) {
+    return "You must choose at least one option."
+  }
+  console.log(combinedChars)
+ 
   const random = new Random();
   let password = ""
   password[0] = lowerCaseLetters.charAt(random.nextInt(lowerCaseLetters.length()));
